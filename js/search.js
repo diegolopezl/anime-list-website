eca86f226a54a3cb7337f3078c03f18d4f12c8ad
Diego Lopez <diegolopezlatour@gmail.com>
2023-03-01 12:07:13 -0400

    Initial commit
    This is the first commit, started making the header for the website and made a working search filter, for the search page

---
+++ js/search.js
@@ -0,0 +1,36 @@
+$.getJSON("./anime.json", function (data) {
+  const animeContainer = $("#anime-container");
+
+  function filterAnime(query) {
+    animeContainer.empty();
+
+    const filteredData = data.filter((anime) => {
+      return anime.title.toLowerCase().includes(query.toLowerCase());
+    });
+
+    if (filteredData.length === 0) {
+      animeContainer.append($("<p>").text("No results found."));
+      return;
+    }
+
+    filteredData.forEach((anime) => {
+      const card = $("<div>").addClass("card");
+      const image = $("<img>")
+        .attr("src", "./imagenes/favicon.png")
+        .addClass("card-img");
+      const text = $("<div>").addClass("text-card");
+      const title = $("<h2>").text(anime.title);
+      const synopsis = $("<p>").text(anime.synopsis);
+      text.append(title, synopsis);
+      card.append(image, text);
+      animeContainer.append(card);
+    });
+  }
+
+  $("#search-bar").on("input", function () {
+    const query = $(this).val();
+    filterAnime(query);
+  });
+
+  filterAnime("");
+});
